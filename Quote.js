1ccb41113d14675307fde471f001d7879804a5a5
Tenrys <tenrys.iaido@live.fr>
2019-07-17 20:37:55 +0200

    Okay this was a waste of time

--- Quote.js
+++ Quote.js
@@ -1,12 +1,5 @@
 const Plugin = require("../plugin");
-const {
-    React,
-    React: { createElement: e },
-} = EDApi;
-
-/* Current issues:
-    - Channel history for fetched message jumps around (HOW DO I FIX THIS)
-*/
+const { React } = EDApi;
 
 module.exports = new Plugin({
     name: "Quotes",
@@ -35,46 +28,15 @@ module.exports = new Plugin({
         `
         );
 
-        const [getChannel, getMessage, fetchMessages] = [
-            findModule("getChannel").getChannel,
-            findModule("getMessage").getMessage,
-            findModule("fetchMessages").fetchMessages,
-        ];
+        const getChannel = findModule("getChannel").getChannel;
 
-        const toReRender = {};
-        findModule("subscribe").subscribe("LOAD_MESSAGES_SUCCESS", data => {
-            for (const message of data.messages) {
-                const component = toReRender[message.id];
-                if (component) {
-                    component.forceUpdate();
-                    delete toReRender[message.id];
-                }
-            }
-        });
+        const request = window.require("request");
 
-        /* None of this seems to be doing anything */
-        monkeyPatch(findModule("dispatch"), "dispatch", data => {
-            if (
-                data.methodArguments[0].type == "LOAD_MESSAGES_SUCCESS" &&
-                typeof data.methodArguments[0].jump == "object" &&
-                data.methodArguments[0].jump.jump == false
-            ) {
-                data.methodArguments[0].jump = undefined;
-            }
-            return data.callOriginalMethod();
-        });
-        monkeyPatch(findModule("maybeDispatch"), "maybeDispatch", data => {
-            if (
-                data.methodArguments[0].type == "LOAD_MESSAGES" &&
-                typeof data.methodArguments[0].jump == "object" &&
-                data.methodArguments[0].jump.jump == false
-            ) {
-                data.methodArguments[0].jump = undefined;
-            }
-            return data.callOriginalMethod();
-        });
+        const _messages = {};
 
         monkeyPatch(findModule("Message").Message.prototype, "render", function(data) {
+            if (data.thisObject.props.embeddedMessage) return data.callOriginalMethod();
+
             const render = data.callOriginalMethod();
 
             const isCompact = /Compact/.test(render.props.className);
@@ -82,29 +44,43 @@ module.exports = new Plugin({
             if (content) {
                 if (!content.props.children) content.props.children = [];
                 const messageEl = isCompact ? content.props.children[1] : content.props.children[0];
-                if (messageEl.props.message && messageEl.props.message.contentParsed) {
+                if (messageEl.props.message && Array.isArray(messageEl.props.message.contentParsed)) {
                     const parsed = messageEl.props.message.contentParsed;
                     for (const item of parsed) {
                         if (typeof item !== "string" && item.props.href) {
                             const [_, channelId, messageId] = item.props.href.match(/https?:\/\/discordapp.com\/channels\/\d*\/(\d*)\/(\d*)\/?/) || [];
                             if (channelId && messageId) {
-                                const [channel, message] = [getChannel(channelId), getMessage(channelId, messageId)];
-                                if (message) {
-                                    /* This doesn't work, fuck cloned elements
-                                    content.props.children.push(
-                                        e(findModule("Message").Message, {
-                                            message,
-                                            channel,
-                                            isHeader: true,
-                                            disableJumpFlash: true,
-                                            className: "embedded-message",
-                                        })
+                                const channel = getChannel(channelId);
+                                if (!_messages[messageId])
+                                    request(
+                                        `https://discordapp.com/api/v6/channels/${channelId}/messages?limit=1&around=${messageId}`,
+                                        {
+                                            headers: { Authorization: ED.localStorage.token.match(/"(.*)"/)[1] },
+                                        },
+                                        (err, res, body) => {
+                                            const message = JSON.parse(body)[0];
+                                            _messages[message.id] = message;
+                                        }
                                     );
-                                    */
-                                } else if (channel) {
-                                    fetchMessages(channelId, null, null, 1, { messageId, jump: false });
-                                    toReRender[messageId] = data.thisObject;
-                                }
+                                else
+                                    content.props.children.push(
+                                        React.createElement("div", { className: "embedded-message" }, [
+                                            React.createElement(findModule("Message").Message, {
+                                                message: _messages[messageId],
+                                                channel,
+                                                isHeader: true,
+                                                disableJumpFlash: true,
+                                                embeddedMessage: true,
+                                                renderAccessories: data.thisObject.props.renderAccessories,
+                                                renderPopoutUser: data.thisObject.props.renderPopoutUser,
+                                                onContextMenuMessage: () => {},
+                                                onContextMenuUser: data.thisObject.props.onContextMenuUser,
+                                                onConfirmDelete: data.thisObject.props.onConfirmDelete,
+                                                onClickUsername: data.thisObject.props.onClickUsername,
+                                                onResize: data.thisObject.props.onResize,
+                                            }),
+                                        ])
+                                    );
                             }
                         }
                     }
---
+++ README.md
@@ -0,0 +1,4 @@
+# Plugins
+
+- **Quote.js**
+    - Meant to display links to Discord messages as embeds but didn't manage to get it working properly at all.
